9775d98cfc1305fc66b0a29502b507bbaa58b024
이연승 <leeys951006@gmail.com>
2024-08-19 15:19:52 +0900

    next.config 복구

--- my-nextjs-app/next.config.mjs
+++ my-nextjs-app/next.config.mjs
@@ -1,5 +1,4 @@
-// next.config.mjs
-export default {
-  reactStrictMode: true,
-  distDir: 'out', // 빌드된 파일들이 저장될 디렉토리
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
